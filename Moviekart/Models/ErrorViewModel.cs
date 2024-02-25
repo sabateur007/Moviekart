7fa6bc9d35a4c634a525ba0f01ad7883beda2a9d
sabateur007 <aniketsingh0107@gmail.com>
2024-02-25 14:51:03 +0530

    Add project files.

---
+++ Moviekart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Moviekart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
