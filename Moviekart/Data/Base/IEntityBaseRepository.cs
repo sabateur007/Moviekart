3c876decbed3d9aea9078dd13026771b4a410563
sabateur007 <aniketsingh0107@gmail.com>
2024-06-09 22:55:23 +0530

    interface for entity base written

---
+++ Moviekart/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Moviekart.Models;
+
+namespace Moviekart.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> GetById(int id);
+        void Add(T entity);
+        Task<T> Update(int id, T entity);
+        Task Delete(int id);
+    }
+}
